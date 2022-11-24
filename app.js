309f5a6e91755c34730356c79dfc158d463a2700
Vladislav Krutikov <Eagleheard5@gmail.com>
2022-11-24 19:53:32 +0300

    fix: fix cors

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const auth = require("./auth");
 dbConnect();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://rick-and-morty-delta-wheat.vercel.app"
+  );
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
